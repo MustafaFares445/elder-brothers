58dfc6b6351b1f6f6efe9ad363f9883e1e7ba2fc
Mustafa Fares <mustaphafares445@gmail.com>
2026-07-29 19:25:07 +0300

    refactor: remove manual subscription creation

--- app/Filament/Resources/CourseSubscriptions/Pages/ListCourseSubscriptions.php
+++ app/Filament/Resources/CourseSubscriptions/Pages/ListCourseSubscriptions.php
@@ -3,15 +3,9 @@
 namespace App\Filament\Resources\CourseSubscriptions\Pages;
 
 use App\Filament\Resources\CourseSubscriptions\CourseSubscriptionResource;
-use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListCourseSubscriptions extends ListRecords
 {
     protected static string $resource = CourseSubscriptionResource::class;
-
-    protected function getHeaderActions(): array
-    {
-        return [CreateAction::make()];
-    }
 }
