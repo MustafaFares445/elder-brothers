48d698221366e97f1f3b0be9e789869905fa65c7
Mustafa Fares <mustaphafares445@gmail.com>
2026-07-29 18:26:35 +0300

    feat: add Arabic dashboard empty states

---
+++ lang/ar/dashboard_empty.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'expiring_subscriptions' => 'لا توجد اشتراكات تنتهي خلال 7 أيام',
+    'expiring_subscriptions_description' => 'ستظهر هنا الاشتراكات النشطة التي اقترب تاريخ انتهائها.',
+];
