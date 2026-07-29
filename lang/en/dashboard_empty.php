cf97a6215eb6ffde06d193f98157e7a468377c4e
Mustafa Fares <mustaphafares445@gmail.com>
2026-07-29 18:26:44 +0300

    feat: add English dashboard empty states

---
+++ lang/en/dashboard_empty.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'expiring_subscriptions' => 'No subscriptions expire within 7 days',
+    'expiring_subscriptions_description' => 'Active subscriptions approaching their expiration date will appear here.',
+];
